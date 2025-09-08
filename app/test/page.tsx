5d2d094e79e092caf109578b87ed47de69ca2204
Jeremy LaFaver <jlafaver@ad.utahmmc.com>
2025-09-08 09:11:56 -0600

    Add test page to debug deployment

---
+++ app/test/page.tsx
@@ -0,0 +1,14 @@
+export default function TestPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold text-gray-900 mb-4">
+          Test Page Working!
+        </h1>
+        <p className="text-lg text-gray-600">
+          If you can see this, the deployment is working correctly.
+        </p>
+      </div>
+    </div>
+  )
+}
