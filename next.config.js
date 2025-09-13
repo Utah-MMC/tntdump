13f58909ba7c984cb88a5a53b6bf2b0ff152fdc4
uwgadmin <lafaverspam@gmail.com>
2025-09-13 14:43:30 -0600

    Set canonical URL to tntdump.com and redirect www to non-www

--- next.config.js
+++ next.config.js
@@ -3,6 +3,21 @@ const nextConfig = {
   images: {
     domains: ['images.unsplash.com', 'via.placeholder.com'],
   },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.tntdump.com',
+          },
+        ],
+        destination: 'https://tntdump.com/:path*',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
