568ba5f8926c83d6ebda116bbf5b3ee6e37d3dde
davIbus <mdavidcall@gmail.com>
2025-11-27 14:53:10 -0700

    Add site config for shared King Tut scripts

---
+++ utah-mmc-site.config.mjs
@@ -0,0 +1,10 @@
+// mmc-site.config.mjs
+export default {
+    siteId: 'tntdump',
+    siteName: 'TNT Dumpsters',
+    domain: 'tntdump.com',
+  
+    // paths relative to the repo root
+    citiesCsvPath: 'data/cities/cities.csv',
+  };
+  
