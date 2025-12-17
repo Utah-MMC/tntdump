8d6c82baaf868278acb80aadd922b5ab9dd767c8
uwgadmin <lafaverspam@gmail.com>
2025-12-17 15:24:31 -0700

    Add Microsoft Clarity tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -130,6 +130,14 @@ export default function RootLayout({
           />
         )}
         {/* Note: GA4 (G-PRG0NC3ZHB) should be configured in GTM to avoid loading duplicate scripts */}
+        {/* Microsoft Clarity tracking code for https://tntdump.com/ */}
+        <Script id="ms-clarity" strategy="lazyOnload">
+          {`(function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "un55u7bpa6");`}
+        </Script>
         {/* Organization Schema - Helps Google identify site name */}
         <script
           type="application/ld+json"
